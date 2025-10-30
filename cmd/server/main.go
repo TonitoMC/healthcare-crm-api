1a4fdc5d0b39d0b0144bb6c745566c24ae1a3c48
TonitoMC <joseameridac@gmail.com>
2025-10-29 20:29:57 -0600

    feat: update dockerfile & add .env.dev

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello from Healthcare CRM backend!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
